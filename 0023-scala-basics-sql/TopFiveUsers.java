1bf26e2cbb6137f6edb6ab5f0f5a573259c5e0fb
Vijay Khanna <54074091+vijay-khanna@users.noreply.github.com>
2020-07-12 00:05:00 +0530

    Add files via upload

--- 0023-scala-basics-sql/TopFiveUsers.java
+++ 0023-scala-basics-sql/TopFiveUsers.java
@@ -1,5 +1,7 @@
 package sparksql;
-
+/*
+ * findTop 5 users who rate movies. 
+ */
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.DataFrameReader;
@@ -11,16 +13,20 @@ public class TopFiveUsers {
 	
 	public static void main(String[] args) {
 		Logger.getLogger("org").setLevel(Level.ERROR);
+		// changed the logging level to reduce the errors on console. 
+		
 		SparkSession session = SparkSession.builder().appName("Top5Users").master("local[*]").getOrCreate();
 		
         DataFrameReader dataFrameReader = session.read();
-        Dataset<Row> ratings = dataFrameReader.option("header","true").csv("in/u.data"); 
+        Dataset<Row> ratings = dataFrameReader.option("header","true").csv("D:\\temp\\in\\userRatingsTransactions.data"); 
+        //Dataset<row> ratings = dataFrameReader.option("header","true").csv(args[0]);
+        // In case we want to push the inputfilename from command line. 
         
         // UserID, ItemID, Rating, TimeStamp
         
-        Dataset<Row> ratingsLean = ratings.select(ratings.col("UserID"));
+        Dataset<Row> ratings_OnlyUserID = ratings.select(ratings.col("UserID"));
         
-        Dataset<Row> groupedData = ratingsLean.groupBy("UserID").count();   
+        Dataset<Row> groupedData = ratings_OnlyUserID.groupBy("UserID").count();   
         Dataset<Row> sortedData = groupedData.orderBy(groupedData.col("count").desc()); 
         sortedData.show(5);
 	}
